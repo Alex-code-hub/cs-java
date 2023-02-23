1f6fe5828901e59b684e6b9bfd88b4fe4bfbaf94
Alex-code-hub <75698907+Alex-code-hub@users.noreply.github.com>
2023-02-23 14:17:13 +0100

    Update EasyExercises.java

--- EasyExercises.java
+++ EasyExercises.java
@@ -15,3 +15,28 @@ class Main {
     }
   }
 }
+
+//2
+import java.util.Scanner;  
+
+class Main {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in); 
+    System.out.print("Enter number: ");
+    int firstNumber = myObj.nextInt();
+    System.out.print("Enter number: ");
+    int secondNumber = myObj.nextInt();
+    System.out.print("Enter number: ");
+    int thirdNumber = myObj.nextInt();
+
+    System.out.println("The biggest number is:");
+    if ((firstNumber > secondNumber) && (firstNumber > thirdNumber)) {
+      System.out.println(firstNumber);
+    } else if ((secondNumber > firstNumber) && (secondNumber > thirdNumber)) {
+      System.out.println(secondNumber);
+    } else {
+      System.out.println(thirdNumber);
+    }
+    
+  }
+}
