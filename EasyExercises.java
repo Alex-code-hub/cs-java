10b49c974598e3621f1e52f710047eb2a0915e11
Alex-code-hub <75698907+Alex-code-hub@users.noreply.github.com>
2023-02-23 16:51:29 +0100

    Update EasyExercises.java

--- EasyExercises.java
+++ EasyExercises.java
@@ -40,3 +40,25 @@ class Main {
     
   }
 }
+
+// 3
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String args[]) {
+    Scanner myObj = new Scanner(System.in); 
+    System.out.print("Enter string: ");
+    String userInput = myObj.nextLine();
+    String reverseInput = "";
+
+    for (int i = userInput.length() - 1; i >= 0; i--) {
+      reverseInput += userInput.charAt(i);
+    }
+
+    if (reverseInput.equals(userInput)) {
+      System.out.println(userInput + " is palindrome");
+    } else {
+      System.out.println(userInput + " is not palindrome");
+    }
+  }
+}
